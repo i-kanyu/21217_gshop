f14c5809433bffd0375d924881cd13f7a1bad5ab
i-kanyu <1452853822@qq.com>
2021-02-17 22:45:27 +0800

    init gshop

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// 入口JS
+
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+new Vue({
+  el: "#app",
+  components: { App },
+  template: "<App/>",
+  router,
+});
